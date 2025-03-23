e9df54539801a20b3a41a5d549b98a9bfdf933f7
Mahir-Neema <mohi10503@gmail.com>
2025-03-23 22:32:14 +0530

    fix

--- frontend/src/lib/firebase.tsx
+++ frontend/src/lib/firebase.tsx
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 // import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCr7NpJo14vKumSJZAPS_YMXce9J_Cyv9E",
-  authDomain: "smiling-bricks.firebaseapp.com",
-  projectId: "smiling-bricks",
-  storageBucket: "smiling-bricks.firebasestorage.app",
-  messagingSenderId: "809654090015",
-  appId: "1:809654090015:web:7637fdadf01b104330d82a",
-  measurementId: "G-TX7SE8VBHG"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
