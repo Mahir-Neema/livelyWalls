8620aa396196d39fd2e54dcb35368728c1fe7db4
Mahir-Neema <mohi10503@gmail.com>
2025-07-03 19:29:15 +0530

    feat: Login enhancement

--- backend/routes/auth.go
+++ backend/routes/auth.go
@@ -6,9 +6,9 @@ import (
 	"github.com/gorilla/mux"
 )
 
-
 func RegisterAuthRoutes(r *mux.Router) {
 	authRouter := r.PathPrefix("/auth").Subrouter()
+	authRouter.HandleFunc("/isUserRegistered", controllers.IsUserRegistered).Methods("POST")
 	authRouter.HandleFunc("/google", controllers.GoogleSignIn).Methods("POST") // Placeholder
 	authRouter.HandleFunc("/signup", controllers.Signup).Methods("POST")
 	authRouter.HandleFunc("/login", controllers.Login).Methods("POST")
